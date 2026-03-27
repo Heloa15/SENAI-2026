df3491529d03a7ccbdccfc1359ebdd0b3e1863d5
Heloá Vitória de Oliveira <godoyheloa9@gmail.com>
2026-03-27 14:59:22 -0300

    Delete PFE 1 directory

--- PFE 1/Atividade Aula 01/script3.js
+++
@@ -1,5 +0,0 @@
-function calcular() {
-            let valor = Number(document.getElementById("valor").value);
-            let desconto = valor > 200 ? valor * 0.05 : 0;
-            resultado.innerHTML = `Desconto: R$ ${desconto.toFixed(2)}<br>Total: R$ ${(valor - desconto).toFixed(2)}`;
-        }
--- PFE 1/Atividade Aula 01/script6.js
+++
@@ -1,7 +0,0 @@
-function calcular() {
-        let valor = Number(document.getElementById("valor").value);
-        let cashback = valor > 300 ? valor * 0.05 : 0;
-        let liquido = valor - cashback;
-        document.getElementById("resultado").innerHTML =
-            `Cashback: R$ ${cashback.toFixed(2)}<br>Valor Líquido: R$ ${liquido.toFixed(2)}`;
-    }
